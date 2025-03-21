350f5f583cad7421419cdbce6a1f2dc80d6d5f84
최지영 <jiyoung@choejiyeong-ui-MacBookAir.local>
2025-03-21 17:40:33 +0900

    메뉴 버튼 생성

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -4,7 +4,12 @@ import React from 'react';
 const Home = () => {
     return (
         <div>
-            메인 화면 입니다. 
+            <div className="menu common-flex">
+                <div className="menu-line1"></div>
+                <div className="menu-line2"></div>
+                <div className="menu-line3"></div>
+            </div>
+            메인 홈 화면 입니다.
         </div>
     )
 }
