2ddf7360c8584c04f09d3477a24b18babe23b191
최지영 <jiyoung@choejiyeong-ui-MacBookAir.local>
2025-03-21 18:34:15 +0900

    메뉴버튼을 모든 페이지에서 보이도록 app.js로 이동함

--- src/App.js
+++ src/App.js
@@ -5,16 +5,23 @@ import Home from './pages/Home/Home';
 import About from './pages/About/About';
 import Project from './pages/Project/Project';
 import Menu from './pages/Menu/Menu';
-
+import { Link } from 'react-router-dom';
 
 function App() {
   return (
     <Router>
-      <Menu/>
+      <Link to="/menu">
+        <div className="menu common-flex">
+          <div className="menu-line1"></div>
+          <div className="menu-line2"></div>
+          <div className="menu-line3"></div>
+        </div>
+      </Link>
       <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/about" element={<About/>}/>
-        <Route path="/project" element={<Project/>}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/project" element={<Project />} />
+        <Route path="/menu" element={<Menu />} />
       </Routes>
     </Router>
   );
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,14 +1,12 @@
 import './Home.css';
-import React from 'react';
+import React, { useState } from 'react';
+import Menu from '../Menu/Menu';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
     return (
         <div>
-            <div className="menu common-flex">
-                <div className="menu-line1"></div>
-                <div className="menu-line2"></div>
-                <div className="menu-line3"></div>
-            </div>
+            
             메인 홈 화면 입니다.
         </div>
     )
