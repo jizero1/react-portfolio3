7b48faa112fa1e7a97db89e161690568b2d71587
최지영 <jiyoung@choejiyeong-ui-MacBookAir.local>
2025-03-21 20:30:19 +0900

    menu.js에 들어오면 마우스 커서를 별로 변경

--- src/pages/Menu/Menu.js
+++ src/pages/Menu/Menu.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Menu.css';
 import { Link } from 'react-router-dom';
 import { FaGithub, FaEnvelope, FaStickyNote } from "react-icons/fa";
@@ -17,16 +17,20 @@ const MenuNav = () => {
 const MenuContact = () => {
     return (
         <div className="menu-contact common-flex">
-            <FaEnvelope className="common-icon"/>
-            <FaStickyNote className="common-icon"/>
-            <FaGithub className="common-icon"/>
+            <FaEnvelope className="common-icon" />
+            <FaStickyNote className="common-icon" />
+            <FaGithub className="common-icon" />
         </div>
     )
 }
 const Menu = () => {
+    const [mouseHover, setMouseHover] = useState(false);
+
     return (
-        <div className="menu-container common-flex">
-            <MenuNav/>
+        <div className={`menu-container common-flex ${mouseHover ? 'custom-cursor-hover' : 'custom-cursor'}`}
+            onMouseEnter={() => setMouseHover(true)}
+            onMouseLeave={() => setMouseHover(false)}>
+            <MenuNav />
             <MenuContact />
         </div>
     )
