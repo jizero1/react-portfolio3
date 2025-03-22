039234057254d9614a205ecf2e45df865ce2c72d
최지영 <jiyoung@choejiyeong-ui-MacBookAir.local>
2025-03-23 00:13:42 +0900

    menu아이콘을 클릭했을때와 안했을때 반복되는 div를줄임(중간 선만 사라지거나 추가되면 되니까 이부분만 렌더링되게함)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route, useNavigate, useLocation } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import About from './pages/About/About';
@@ -13,34 +13,40 @@ function MenuIcon() {
   const location = useLocation();
 
 
+  const [menuIcon, setMenuIcon] = useState(true);
   // 메뉴 클릭시
   const handleMenuClick = () => {
     // 현재 경로가 /menu에 있을경우, 이전 페이지로 이동한다.
     if (location.pathname === '/menu') {
       navigate(-1); // 뒤로가기
+      setMenuIcon(false);
     } else {
       navigate('/menu');
-      // 메뉴창에 들어오면, 이모티콘을 x로 변경한다
     }
   };
 
+  // 메뉴페이지에 들어오면 메뉴아이콘을 X로 변경
+  useEffect(() => {
+    if (location.pathname === '/menu') {
+      setMenuIcon(false); // 메뉴 페이지에 있을 때
+      // 메뉴페이지에 있을때는 menu에 바탕색을 흰색으로 변경함.
+    } else {
+      setMenuIcon(true); // 메뉴 페이지가 아닐 때
+    }
+  }, [location.pathname]); // location.pathname이 변경될 때마다 실행
+
+  const menuColor = {
+    backgroundColor: menuIcon === false ? 'white' : 'black',
+  };
+
+
   return (
     <div className="menu common-flex" onClick={handleMenuClick}>
-      {menuClose && (
         <div>
-
-        <div className="menu-line1"></div>
-      <div className="menu-line2"></div>
-      <div className="menu-line3"></div>
-      </div>
-      )}
-      {!menuClose && (
-                <div>
-                <div className="menu-line1"></div>
-              <div className="menu-line3"></div>
-              </div>
-      )}
-
+          <div className="menu-line1 common-menu-line"></div>
+          {menuIcon && (<div className="menu-line2 common-menu-line"></div>)}
+          <div className="menu-line3 common-menu-line"></div>
+        </div>
     </div>
   );
 }
