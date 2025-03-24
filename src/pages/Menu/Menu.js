916ffbe98106738853bbcab2d0d4d85fd9891dc3
최지영 <jiyoung@choejiyeong-ui-MacBookAir.local>
2025-03-24 20:22:39 +0900

    navigator.clipboard.writeText로 링크복사기능 구현

--- src/pages/Menu/Menu.js
+++ src/pages/Menu/Menu.js
@@ -2,9 +2,8 @@ import React, { useState } from 'react';
 import './Menu.css';
 import { Link } from 'react-router-dom';
 import { FaGithub, FaEnvelope, FaStickyNote } from "react-icons/fa";
-// import { useDispatch, useSelector } from 'react-redux';
-// import { toggleMenu } from '../../actions/menuActions';
 import { useLocation, useNavigate } from 'react-router-dom';
+import { FaCopy } from "react-icons/fa6";
 
 
 const MenuNav = () => {
@@ -25,6 +24,7 @@ const MenuContact = () => {
     const [menuLinkOpen, setMenuLinkOpen] = useState(false);
     const handleMenuLink = (text) => {
         setMenuLinkOpen(!menuLinkOpen);
+        setMenuLinkText(text);
         // menuLinkOpen이 true인 상태에서 새로운 text가 들어오면 false로 변경하지 않는다
         if (menuLinkOpen === true) {
             if (menuLinkText !== text) {
@@ -32,6 +32,22 @@ const MenuContact = () => {
                 setMenuLinkText(text);
             }
         }
+    }
+
+    // 현재 복사가 되었는지 상태 저장 변수
+    // const [copy, setCopy] = useState('');
+
+    // 컨택메뉴에서 복사버튼 클릭시 링크 복사 함수
+    const handleCopy = () => {
+        // 복사하려는 url = menuLinkText
+        navigator.clipboard.writeText(menuLinkText)
+        .then(() => {
+            alert("링크가 복사되었습니다.");
+            console.log("링크복사 성공");
+        })
+        .catch(() => {
+            console.log("링크복사 실패");
+        });
     }
     return (
         <div className="menu-contact-container common-flex">
@@ -42,8 +58,9 @@ const MenuContact = () => {
             </div>
             {/* // 클릭한 메뉴에따라 삼각형 위치도 달라져야함 */}
             {menuLinkOpen && (
-                <div className="menuLink common-flex">
-                    <p>{menuLinkText}</p>
+                <div className="menuLinkContainer common-flex">
+                    <p className="menuLink">{menuLinkText}</p>
+                    <button onClick={handleCopy} className="menuLink-copyBtn common-flex"><FaCopy/></button>
                 </div>
             )}
         </div>
