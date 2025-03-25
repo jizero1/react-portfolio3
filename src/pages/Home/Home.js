f64ed4c2232a4f588780d4e8c5ea0a05ec7ce0ca
최지영 <jiyoung@choejiyeong-ui-MacBookAir.local>
2025-03-25 20:59:17 +0900

    home.js 스크롤 아이콘 넣고 애니메이션 추가

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -4,11 +4,17 @@ import Menu from '../Menu/Menu';
 import { Link } from 'react-router-dom';
 import { motion } from "framer-motion";
 import { ReactTyped } from 'react-typed';
+import { FaAngleDown } from "react-icons/fa";
 
 const HomeText = () => {
 
+    const [scrollIcon, setScrollIcon] = useState(false);
+    const handleTypingComplete = () => {
+        setScrollIcon(true);
+    }
     return (
-        <div className="home-text common-flex">
+        <div className="home-text-container common-flex">
+            <div className="home-text">
             <ReactTyped
                 strings={["HELLO, I'M JIYOUNG", "FRONT-END DEVELOPER", "VIEW MY PORTFOLIO"]}
                 typeSpeed={100} // 타이핑 속도
@@ -16,7 +22,12 @@ const HomeText = () => {
                 backDelay={1200} // 타이핑 후 잠시 대기
                 startDelay={500} // 시작 지연
                 loop={false} // 무한 반복 설정
+                onComplete={handleTypingComplete}
             />
+            </div>
+            {scrollIcon && (
+                <FaAngleDown className="home-scrollIcon"/>
+            )}
         </div>
     )
 }
