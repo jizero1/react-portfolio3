e8def026d9ad56d7ac4a91498c8d8ea9cb0c988a
최지영 <jiyoung@choejiyeong-ui-MacBookAir.local>
2025-03-26 20:45:12 +0900

    home화면 텍스트 타이핑코드를 컴포넌트로 따로 만듦

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -4,37 +4,89 @@ import Menu from '../Menu/Menu';
 import { Link } from 'react-router-dom';
 import { motion } from "framer-motion";
 import { ReactTyped } from 'react-typed';
-import { FaAngleDown } from "react-icons/fa";
+import { FaAngleDown, FaStar } from "react-icons/fa";
 
-const HomeText = () => {
+const HomeStarIcon = ({ scrollIcon }) => {
 
-    const [scrollIcon, setScrollIcon] = useState(false);
-    const handleTypingComplete = () => {
-        setScrollIcon(true);
+    // 스크롤아이콘이 뜨면 돋보기와 별 아이콘(이미지)띄운다.
+    const [starIconLeft, setStarIconLeft] = useState(false);
+    const [starIconRight, setStarIconRight] = useState(false);
+
+    useEffect(() => {
+        if (scrollIcon) {
+            setStarIconLeft(true);
+            setStarIconRight(true);
+        }
+    }, [scrollIcon]);
+
+    return (
+        <div className="home-starIcon-container">
+            <div className="home-starIcon-left common-home-starIcon" style={{ opacity: scrollIcon ? 1 : 0 }} >
+                <FaStar />
+            </div>
+            <div className="home-starIcon-right common-home-starIcon" style={{ opacity: scrollIcon ? 1 : 0 }} >
+                <FaStar />
+            </div>
+        </div>
+    )
+}
+
+const TextTyping = ({ text, onComplete }) => {
+    return (
+        <ReactTyped
+            strings={[text]}
+            typeSpeed={100}
+            startDelay={500}
+            loop={false}
+            showCursor={false}
+            onComplete={onComplete}
+        >
+        </ReactTyped>
+    )
+}
+
+const HomeText = ({ scrollIcon, handleTypingComplete }) => {
+    const [firstText, setFirstText] = useState(false);
+    const [secondText, setSecondText] = useState(false);
+    const handleFirstText = () => {
+        setFirstText(true);
+    }
+    const handleSecondText = () => {
+        setSecondText(true);
+    }
+    const nextText = (next) => {
+        next(true);
+    }
+    const handleThirdText = () => {
+
     }
     return (
         <div className="home-text-container common-flex">
             <div className="home-text">
-            <ReactTyped
-                strings={["HELLO, I'M JIYOUNG", "FRONT-END DEVELOPER", "VIEW MY PORTFOLIO"]}
-                typeSpeed={100} // 타이핑 속도
-                backSpeed={70} // 지우는 속도
-                backDelay={1200} // 타이핑 후 잠시 대기
-                startDelay={500} // 시작 지연
-                loop={false} // 무한 반복 설정
-                onComplete={handleTypingComplete}
-            />
+                <TextTyping text={"HELLO, I'M JIYOUNG"} onComplete={()=> nextText(setFirstText)}></TextTyping>
+                {firstText && (
+                    <TextTyping text={"FRONT-END DEVELOPER"} onComplete={() => nextText(setSecondText)}></TextTyping>
+                )}
+                {secondText && (
+                    <TextTyping text={"VIEW MY PORTFOLIO"} onComplete={handleThirdText}></TextTyping>
+            
+                )}
             </div>
             {scrollIcon && (
-                <FaAngleDown className="home-scrollIcon"/>
+                <FaAngleDown className="home-scrollIcon" />
             )}
         </div>
     )
 }
 const Home = () => {
+    const [scrollIcon, setScrollIcon] = useState(false);
+    const handleTypingComplete = () => {
+        setScrollIcon(true);
+    }
     return (
         <div className="home-container common-flex">
-            <HomeText />
+            <HomeStarIcon scrollIcon={scrollIcon} />
+            <HomeText scrollIcon={scrollIcon} handleTypingComplete={handleTypingComplete} />
         </div>
     )
 }
